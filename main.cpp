6fec44f7df2a786a93308a5e05af53b22c61e37d
CreatorsDevs <44081388+CreatorsDevs@users.noreply.github.com>
2023-02-23 14:49:46 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int playerHealth;
+  int *p;
+
+  cout << "\nEnter the player Health: ";
+  cin >> playerHealth;
+  
+  p = &playerHealth;
+  cout << "Value of p is: " << p << endl;
+  cout << "Value of Player Health using pointer is: " << *p << endl;
+  cout << "Value of player Health as it is: " << playerHealth << endl;
+
+  cout << "\nChange the value of player health: ";
+  cin >> *p;
+
+  cout << "Value of p is: " << p << endl;
+  cout << "Value of Player Health using pointer is: " << *p << endl;
+  cout << "Value of player Health as it is: " << playerHealth << endl;
+
+  return 0;
+}
